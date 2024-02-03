5a10c6cad115b81331157bdce063c449370c3805
Chris Hildebrand <hildebrandc94@gmail.com>
2024-02-03 12:32:12 -0500

    First commit

---
+++ backend/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Childebrand94/takeHomePhoneNumber/pkg/handler"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	queryHandler := &handler.Query{}
+	r.Post("/submit", queryHandler.Parse)
+	http.ListenAndServe(":3000", r)
+}
